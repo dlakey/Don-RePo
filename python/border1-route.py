d68defcb195cd37a2dfa6618e31562c1c093b7a6
dlakey <lakesystem@gamil.com>
2017-10-20 08:40:31 -0700

    Update

--- python/border1-route.py
+++ python/border1-route.py
@@ -5,7 +5,7 @@ from pprint import pprint
 from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 from jnpr.junos.op.route import routes
-r0 = Device(host='172.16.155.254',user='root',password='Juniper').open()
+r0 = Device(host='172.16.1.250',user='root',password='Juniper').open()
 r0.bind(route_table=routes)
 r0.cu
 route_table = routes(r0)
